978009d11f96d9ecfb7205cd579de6fa413c64b8
Allan Regush <allan@allanregush.com>
2022-08-08 17:56:28 -0600

    Feat: upload files

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var cors = require('cors');
+const { application } = require('express');
 require('dotenv').config()
+const multer  = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
 
 var app = express();
 
@@ -11,7 +23,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file');
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  res.send({ name: file.originalname , type: file.mimetype, size: file.size});
+});
 
 
 const port = process.env.PORT || 3000;
